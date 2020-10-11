65a591c7f58acc077ef6d89d8718c7bf40bf5088
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2020-10-11 17:36:31 +0200

    lul, coolest header ever

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,23 @@ import { Link } from 'gatsby';
 
 import './header.scss';
 
+import arrow from '../../assets/arrow.svg';
+
 const Header = () => {
   return (
     <header>
-      <Link activeClassName='active' to='/'>Home</Link>
-      <Link activeClassName='active' to='/about-me'>About Me</Link>
-      <Link activeClassName='active' to='/contact'>Contact me</Link>
+      <Link activeClassName='active' to='/'>
+        <div className='arrow'><img src={arrow} /></div>
+        Home
+      </Link>
+      <Link activeClassName='active' to='/about-me'>
+        <div className='arrow'><img src={arrow} /></div>
+        About Me
+      </Link>
+      <Link activeClassName='active' to='/contact'>
+        <div className='arrow'><img src={arrow} /></div>
+        Contact me
+      </Link>
     </header>
   );
 }
