e0a6ae0b5b410bbbd25aeaabab610c71e0fd0521
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2020-10-11 18:46:31 +0200

    fill it with some words

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -6,6 +6,15 @@ const AboutMe = () => {
   return (
     <Layout>
       <h1>Hi, I'm Dave</h1>
+      <p>
+        My full name is David Vodrážka and one of the reasons why I created this website is to see myself when I google my name, which is why I use it quite extensively.
+      </p>
+      <p>
+        I was born and raised in the Czech Republic (it should be Czechia now, but I don't agree), I'm stationed in the capitol - Prague.
+      </p>
+      <p>
+        Programming use to be just something I was learning out of curiosity, but now it pays my bills. I am employed in Seznam.cz, which is a pretty big deal here in the Czeach Republic (no, not Czechia).
+      </p>
     </Layout>
   );
 };
