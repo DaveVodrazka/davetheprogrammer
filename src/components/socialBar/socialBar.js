5d7d227de92981e7da693fcb1f3d2d7402f96395
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2020-10-12 15:15:36 +0200

    update twitter link

--- src/components/socialBar/socialBar.js
+++ src/components/socialBar/socialBar.js
@@ -10,7 +10,7 @@ const SocialBar = () => {
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' />
       </Helmet>
       <a href='https://www.facebook.com/Vydrysek' target='_blank' className='fa fa-facebook'></a>
-      <a href='https://twitter.com/david_vodrazka' target='_blank' className='fa fa-twitter'></a>
+      <a href='https://twitter.com/DaveVodrazka' target='_blank' className='fa fa-twitter'></a>
       <a href='https://github.com/DaveVodrazka' target='_blank' id='githubSocial' className='fa fa-github'></a>
       <a href='https://www.linkedin.com/in/dave-vodr%C3%A1%C5%BEka/' target='_blank' className='fa fa-linkedin'></a>
       <a href='https://www.instagram.com/dave_vydrysek/' target='_blank' className='fa fa-instagram'></a>
