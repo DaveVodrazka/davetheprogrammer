f7363232eb0c5ecf5e50887c1db3eaff794e9e85
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2020-10-13 17:04:25 +0200

    fix: header arrow

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,11 +12,11 @@ const Header = () => {
         <div className='arrow'><img src={arrow} alt='Arrow pointing to the current page - home' /></div>
         Home
       </Link>
-      <Link activeClassName='active' to='/about-me'>
+      <Link activeClassName='active' to='/about-me/'>
         <div className='arrow'><img src={arrow} alt='Arrow pointing to the current page - about me' /></div>
         About Me
       </Link>
-      <Link activeClassName='active' to='/contact'>
+      <Link activeClassName='active' to='/contact/'>
         <div className='arrow'><img src={arrow} alt='Arrow pointing to the current page - contact me' /></div>
         Contact me
       </Link>
